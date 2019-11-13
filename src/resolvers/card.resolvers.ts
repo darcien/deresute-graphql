8bf6a66cd89b8fcbba67fe0c8034e0c14234d5fe
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-11-14 00:47:03 +0700

    add card detail query

--- src/resolvers/card.resolvers.ts
+++ src/resolvers/card.resolvers.ts
@@ -3,5 +3,7 @@ import { fetchAPI } from '../utils';
 export default {
   Query: {
     cards: async () => (await fetchAPI('/list/card_t'))?.result ?? [],
+    card: async (_, { id }) =>
+      ((await fetchAPI(`/card_t/${id}`))?.result ?? [])[0],
   },
 };

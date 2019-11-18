5c94ff5c83daf6e515b3d17b6b0b7ddd0fa44f84
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-11-18 23:22:07 +0700

    add skill and leaderSkill query

--- src/resolvers/card.resolvers.ts
+++ src/resolvers/card.resolvers.ts
@@ -5,5 +5,9 @@ export default {
     cards: async () => (await fetchAPI('/list/card_t'))?.result ?? [],
     card: async (_, { id }) =>
       ((await fetchAPI(`/card_t/${id}`))?.result ?? [])[0],
+    skill: async (_, { id }) =>
+      ((await fetchAPI(`/skill_t/${id}`))?.result ?? [])[0],
+    leaderSkill: async (_, { id }) =>
+      ((await fetchAPI(`/leader_skill_t/${id}`))?.result ?? [])[0],
   },
 };

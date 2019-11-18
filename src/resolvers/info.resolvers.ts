1978b01abaeb27e66a31bb711d1e790269f09bc1
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-11-18 23:51:55 +0700

    add happening query

--- src/resolvers/info.resolvers.ts
+++ src/resolvers/info.resolvers.ts
@@ -3,5 +3,7 @@ import { fetchAPI } from '../utils';
 export default {
   Query: {
     info: () => fetchAPI('/info'),
+    happeningNow: () => fetchAPI('/happening/now'),
+    happening: (_, { timestamp }) => fetchAPI(`/happening/${timestamp}`),
   },
 };

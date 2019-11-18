139c4e95c00f680c836784c927f8251c4ac17ad6
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2019-11-18 23:22:56 +0700

    merge char resolvers with card resolvers

--- src/resolvers/card.resolvers.ts
+++ src/resolvers/card.resolvers.ts
@@ -2,6 +2,9 @@ import { fetchAPI } from '../utils';
 
 export default {
   Query: {
+    chars: async () => (await fetchAPI('/list/char_t'))?.result ?? [],
+    char: async (_, { id }) =>
+      ((await fetchAPI(`/char_t/${id}`))?.result ?? [])[0],
     cards: async () => (await fetchAPI('/list/card_t'))?.result ?? [],
     card: async (_, { id }) =>
       ((await fetchAPI(`/card_t/${id}`))?.result ?? [])[0],
--- src/resolvers/char.resolvers.ts
+++
@@ -1,9 +0,0 @@
-import { fetchAPI } from '../utils';
-
-export default {
-  Query: {
-    chars: async () => (await fetchAPI('/list/char_t'))?.result ?? [],
-    char: async (_, { id }) =>
-      ((await fetchAPI(`/char_t/${id}`))?.result ?? [])[0],
-  },
-};
